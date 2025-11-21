6e22274a56b6f3915036bffd9d4829ff1613483f
Yannn18 <azaryayanuar18@gmail.com>
2025-11-21 09:58:40 +0700

    crystal report1

---
+++ POSBANDI2/cetakpetugas.vb
@@ -0,0 +1,39 @@
+﻿Imports CrystalDecisions
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+
+Public Class cetakpetugas
+    Private Sub CrystalReportViewer1_Load(sender As Object, e As EventArgs) Handles CrystalReportViewer1.Load
+
+    End Sub
+
+    Private Sub cetakpetugas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim rpt As New petugas   ' Ganti dengan nama file rpt Anda
+
+        ' --- BAGIAN PENTING: LOGIN ULANG KE DATABASE ---
+        ' Crystal Report butuh login ulang saat runtime agar bisa tarik data baru
+        Dim connectionInfo As New ConnectionInfo()
+        connectionInfo.ServerName = "petugas" ' Atau DSN Anda
+        connectionInfo.DatabaseName = "posbandi"
+        connectionInfo.UserID = "root"
+        connectionInfo.Password = "" ' Isi jika ada password
+
+        SetDBLogonForReport(connectionInfo, rpt)
+        ' ------------------------------------------------
+
+        ' --- PAKSA REFRESH DATA ---
+        rpt.Refresh()
+
+        CrystalReportViewer1.ReportSource = rpt
+        CrystalReportViewer1.Refresh() ' Refresh juga viewernya
+    End Sub
+
+    Private Sub SetDBLogonForReport(ByVal myConnectionInfo As ConnectionInfo, ByVal myReportDocument As ReportDocument)
+        Dim myTables As Tables = myReportDocument.Database.Tables
+        For Each myTable As Table In myTables
+            Dim myTableLogonInfo As TableLogOnInfo = myTable.LogOnInfo
+            myTableLogonInfo.ConnectionInfo = myConnectionInfo
+            myTable.ApplyLogOnInfo(myTableLogonInfo)
+        Next
+    End Sub
+End Class
